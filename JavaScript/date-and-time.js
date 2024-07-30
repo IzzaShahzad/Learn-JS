e579235718255d21c5cdf7f145986cdf41883820
Izza Shahzad <168627683+IzzaShahzad@users.noreply.github.com>
2024-07-30 09:48:43 +0000

    Dates in JS

---
+++ JavaScript/date-and-time.js
@@ -0,0 +1,32 @@
+
+// Dates in JS
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024, 0, 21)
+// let myCreatedDate = new Date(2024, 0, 13, 5, 8)
+// let myCreatedDate = new Date("2024-01-18")
+let myCreatedDate = new Date("7-29-2024")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
