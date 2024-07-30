c16b6c9434b679c6ad946dae45db6bafbe009457
Izza Shahzad <168627683+IzzaShahzad@users.noreply.github.com>
2024-07-30 08:30:30 +0000

    Operations and Conversions of Datatype

--- JavaScript/ConversionsORoperations.js
+++ JavaScript/ConversionsORoperations.js
@@ -20,3 +20,27 @@ let someNumber = 50;
 let stringNumber = String(someNumber)
 console.log (stringNumber);
 console.log (typeof stringNumber);
+
+
+//**********************************Operations*********************************** */
+let value = 7
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Hello"
+let str2 = "Sunshine"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log((3+4) * 5 % 3);
+
+console.log(+true);
+let a, b, c
+a = b = c = 5 + 5
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
