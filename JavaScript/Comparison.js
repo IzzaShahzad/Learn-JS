dd86e16eaf72491144919524eeb2f7b594d32436
Izza Shahzad <168627683+IzzaShahzad@users.noreply.github.com>
2024-07-30 08:48:30 +0000

    Compariosns

---
+++ JavaScript/Comparison.js
@@ -0,0 +1,13 @@
+console.log("2">1);
+console.log("02">1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0); //it returns true value the reason is that an empty check and comparison operators works differently. Comparison converts null to a number,treating it as 0.Thats why null>=0 is true and null>0 is false.
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0); 
+
+//==== 
+console.log("5" === 5);
+
