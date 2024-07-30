47c82193f141b3bb9a793b51c7fa310d68a85416
Izza Shahzad <168627683+IzzaShahzad@users.noreply.github.com>
2024-07-30 07:57:23 +0000

    Conversions of Datatypes

---
+++ JavaScript/ConversionsORoperations.js
@@ -0,0 +1,22 @@
+let score = "54"
+console.log(typeof (score))
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+//"54" => 54
+//"54abv" =>NaN
+//true => 1 ; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 =>true ; 0 =>false
+// "" => false
+// "Izzah" =>true
+
+let someNumber = 50;
+let stringNumber = String(someNumber)
+console.log (stringNumber);
+console.log (typeof stringNumber);
