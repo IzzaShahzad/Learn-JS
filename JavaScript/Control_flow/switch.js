b90933da4bdd09472f5aab5e11c8f2dba256fe62
Izza Shahzad <168627683+IzzaShahzad@users.noreply.github.com>
2024-08-02 06:49:02 +0000

    Switch Statement in JS

---
+++ JavaScript/Control_flow/switch.js
@@ -0,0 +1,33 @@
+//+++++++++++++++++++++Switch Statement++++++++++++++++++++++++++
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "5"
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+
+    default:
+        console.log("Invalid");
+        break;
+}
